00d49572713123fcf788fb7478b389ecde913c03
Luca <lulu-turrioni@outlook.fr>
2021-07-23 14:28:42 +0200

    0.1

--- src/be/shark_zekrom/ballons/Main.java
+++ src/be/shark_zekrom/ballons/Main.java
@@ -1,14 +1,26 @@
 package be.shark_zekrom.ballons;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
-
+    private static Main instance;
+    public static Main getInstance() {
+        return instance;
+    }
 
     @Override
     public void onEnable() {
+        instance = this;
 
+        Bukkit.getLogger().info("Ballons+ enabled !");
+        PluginManager pm = getServer().getPluginManager();
+
+      //  pm.registerEvents(new Manager(), this);
+
+        //this.getCommand("familier+").setExecutor(new Commands());
     }
 
     @Override
